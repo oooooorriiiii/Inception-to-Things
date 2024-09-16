e3bfc10a883a564ab458ae62d81d65025879ea7c
ymori <yuu.mori06@gmail.com>
2024-09-16 22:34:31 +0900

    upgrade: wil42-playgroundのフォワーディング-> LoadBalancer + k3dのマッピングに変更
    - 以下の問題が解消
      - 32ccde81c431ea975920926050be6b3537733d2d

--- p3/scripts/create-k3d-cluster.sh
+++ p3/scripts/create-k3d-cluster.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-k3d cluster create ymoriCluster
+k3d cluster create ymoriCluster \
+    --port "8888:8888@loadbalancer"
--- p3/scripts/deploy-wil-app.sh
+++ p3/scripts/deploy-wil-app.sh
@@ -5,6 +5,3 @@ kubectl create namespace dev
 kubectl apply -f ./confs/application.yaml
 
 kubectl wait --for=condition=ready pod --all -n dev --timeout=3m
-
-# Port Forwarding
-kubectl port-forward svc/wil42-playground-service -n dev 8888:8888 --address 0.0.0.0 >/dev/null 2>&1 &
