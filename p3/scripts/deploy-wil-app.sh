0835709a45f0d0114169db9def6c59429e2c1b27
ymori <yuu.mori06@gmail.com>
2024-09-16 18:28:52 +0900

    fix: CRLF -> LF

--- p3/scripts/deploy-wil-app.sh
+++ p3/scripts/deploy-wil-app.sh
@@ -7,4 +7,4 @@ kubectl apply -f ./confs/application.yaml
 kubectl wait --for=condition=ready pod --all -n dev --timeout=3m
 
 # Port Forwarding
-kubectl port-forward svc/wil42-playground-service -n dev 8888:8888 --address 0.0.0.0 >/dev/null 2>&1
+kubectl port-forward svc/wil42-playground-service -n dev 8888:8888 --address 0.0.0.0 >/dev/null 2>&1 &
